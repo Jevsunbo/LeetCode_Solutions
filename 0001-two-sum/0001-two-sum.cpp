9269a5ce0d173c4f121426b93cae09036f0cce06
Jeremiah Velasquez <152227173+Jevsunbo@users.noreply.github.com>
2026-06-03 10:03:53 -0700

    Time: 0 ms (100%), Space: 14.7 MB (54.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices;
-
+//         # Approach:
+// # - Check if we've already seen that complement in our map
+// # - If yes → we found our pair, return both indices
+// # - If no → store current number and its index in the map, move on
+        unordered_map<int, int> seen;
+        vector<int> indicies;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = i+1; j < nums.size();j++){
-                if(nums[i] + nums[j] == target){
-                    indices.push_back(i);
-                    indices.push_back(j);
-                }
+// # - For each number, calculate what we NEED (complement = target - num)
+            int complement = target - nums[i];
+
+            if(seen.count(complement)){
+                return {seen[complement], i};
             }
+
+            seen[nums[i]] = i;
         }
-        return indices;
+        return {};
     }
 };
