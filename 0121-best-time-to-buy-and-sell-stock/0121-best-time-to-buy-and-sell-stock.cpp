d4ada986f896b0ae91e678044edd05de987f8058
Jeremiah Velasquez <152227173+Jevsunbo@users.noreply.github.com>
2026-06-02 22:15:30 -0700

    Time: 0 ms (100%), Space: 97.4 MB (24.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,17 +4,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int lowest, highest = 0;
-        // find the lowest price day (buy) and the highest price (profit)
+        int lowest = 0, highest = 0;
         lowest = prices[0];
+
         for(int i = 0; i < prices.size(); i++){
-            // find lowest price
-            if(prices[i] < lowest){
+            if (prices[i]<lowest){
                 lowest = prices[i];
             }
-            // compare that to next elements and find highest profit
-            if(prices[i]-lowest > highest){
-                highest = prices[i]-lowest;
+            if(prices[i] - lowest > highest){
+                highest = prices[i] - lowest;
             }
         }
         return highest;
