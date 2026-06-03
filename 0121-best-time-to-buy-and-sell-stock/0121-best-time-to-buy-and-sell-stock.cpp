202d6d4aca2ee52a1460a23b22d21d5b33cf78fb
Jeremiah Velasquez <152227173+Jevsunbo@users.noreply.github.com>
2026-06-03 09:27:24 -0700

    Time: 1 ms (35.68%), Space: 97.3 MB (86.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,18 @@
-// Problem: Best Time to Buy and Sell Stock (#121)
-// Pattern: Single pass, track running minimum
-// Time: O(n) | Space: O(1)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int lowest = 0, highest = 0;
-        lowest = prices[0];
+        int lowest = prices[0];
+        int highest = 0;
 
-        for(int i = 0; i < prices.size(); i++){
-            if (prices[i]<lowest){
-                lowest = prices[i];
+        for (int i = 0; i < prices.size(); i++){
+            if(prices[i]<lowest){
+                lowest=prices[i];
             }
-            if(prices[i] - lowest > highest){
-                highest = prices[i] - lowest;
+            if(prices[i]-lowest > highest){
+                highest = prices[i]-lowest;
             }
+            
         }
         return highest;
-    };
+    }
 };
