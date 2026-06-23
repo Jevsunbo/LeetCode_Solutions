ae88ff389ed12b977879e8a3a4d46822997bebb5
Jeremiah Velasquez <152227173+Jevsunbo@users.noreply.github.com>
2026-06-22 22:01:47 -0700

    Time: 0 ms (100%), Space: 23.7 MB (98.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int slow = 0;
-
-        for(int fast = 0; fast<nums.size(); fast++){
-            if(nums[fast] != 0){
-            nums[slow] = nums[fast];
-            if(slow != fast){nums[fast] = 0;}
-            slow++;}
+        for (int fast = 0; fast < nums.size(); fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+            }
         }
-    };
+    }
 };
