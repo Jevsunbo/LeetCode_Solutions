000a2fa1fb2fb34e6d059ec8f0c5586d84655553
Jeremiah Velasquez <152227173+Jevsunbo@users.noreply.github.com>
2026-06-24 23:44:10 -0700

    Time: 17 ms (53.49%), Space: 25 MB (79.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-// turn each element of the array into anagrams versions by sorting
-// storing the anagram as a key and putting the words as the value
-// just return the hashmap values
-unordered_map<string, vector<string>> map;
-    for(int i=0; i<strs.size(); i++){
-        string key = strs[i];
-        sort(key.begin(), key.end());
-        map[key].push_back(strs[i]);
-    }
-vector<vector<string>> result;
-    for(auto& key : map){
-        result.push_back(key.second);
-    }
-    return result;
+        unordered_map<string, vector<string>> ani;
+        for(int i =0 ; i<strs.size(); i++){
+            string ana = strs[i];
+            sort(ana.begin(), ana.end());
+            ani[ana].push_back(strs[i]);
+        }
+
+        vector<vector<string>> result;
+        for(auto& answer: ani){
+            result.push_back(answer.second);
+        }
+        return result;
     }
 };
